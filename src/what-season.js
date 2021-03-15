1d407e91260f94715ccecf18f5d455985449a467
MishynPavel <68873173+MishynPavel@users.noreply.github.com>
2021-03-15 17:56:57 -0300

    feat: implement what-season

--- src/what-season.js
+++ src/what-season.js
@@ -1,25 +1,21 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function getSeason(date) {
-  'use strict';
-
-  if (!date) {
-    return 'Unable to determine the time of year!';
+  if(!date) {
+    return "Unable to determine the time of year!";
   }
 
-  if (Object.prototype.toString.call(date) !== "[object Date]" || isNaN(date)) {
-    throw new Error('date argument is invalid');
-  }
+  const month = Date.prototype.getMonth.call(date);
 
-  const month = date.getMonth();
-
-  if (month >= 2 && month <= 4) {
-    return 'spring';
-  } else if (month >= 5 && month <= 7) {
-    return 'summer';
-  } else if (month >= 8 && month <= 10) {
-    return 'autumn';
-  } else {
-    return 'winter';
-  }
+  if (1 < month && month <= 4) {
+    return "spring";
+  } else if (4 < month && month <= 7) {
+    return "summer";
+  } else if (7 < month && month <= 10) {
+    return "autumn";
+  } else if (month === 11 ||
+    month === 0 ||
+    month === 1) {
+    return "winter";
+  }  
 };
