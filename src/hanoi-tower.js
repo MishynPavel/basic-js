f54345f7229ad17184f6ded0ec0c3b5070a71627
MishynPavel <68873173+MishynPavel@users.noreply.github.com>
2021-03-15 17:51:49 -0300

    feat: implement hanoi-tower

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,10 +1,11 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
-  'use strict';
-
-  const turns = 2 ** disksNumber - 1;
-  const seconds = Math.floor(((turns / turnsSpeed) * 3600));
-
-  return {turns, seconds};
+  const turns = Math.pow(2, disksNumber) - 1;
+  const seconds = Math.floor((3600 / turnsSpeed) * turns);
+  const result = {
+    turns: turns,
+    seconds: seconds
+  };
+  return result;
 };
